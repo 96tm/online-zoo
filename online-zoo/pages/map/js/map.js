502b4c874ccd37a50d04ef180a90942e4beaa434
96tm <khabcity@gmail.com>
2021-04-17 13:42:30 +0300

    feat: finish adaptive design for map page

--- online-zoo/pages/map/js/map.js
+++ online-zoo/pages/map/js/map.js
@@ -3,6 +3,7 @@ const tooltip = document.querySelector(".tooltip");
 const header = document.querySelector(".header");
 const y0 = -header.getBoundingClientRect().height;
 const mapMarks = new Set(document.querySelectorAll(".map__mark"));
+let shown = false;
 
 map.addEventListener("mouseover", event => {
   if (event.target.classList.contains("animal-figure")) {
@@ -13,6 +14,7 @@ map.addEventListener("mouseover", event => {
     mark.classList.add("map__mark-active");
     let [x, y] = [figureRect.left, y0 + event.pageY + figureRect.height];
     x -= tooltip.getBoundingClientRect().width / 4;
+    console.log(tooltip.getBoundingClientRect().width / 4);
     const animal = mark.dataset.animal;
     const location = mark.dataset.location;
     tooltip.querySelector(".tooltip__animal").innerText = animal;
@@ -20,5 +22,14 @@ map.addEventListener("mouseover", event => {
     tooltip.style.left = x + "px";
     tooltip.style.top = y + "px";
     tooltip.style.visibility = "visible";
+    shown = true;
+    console.log(x, figureRect.x, event.clientX);
+  }
+});
+
+map.addEventListener("mouseout", event => {
+  if (event.target.classList.contains("animal-figure")) {
+    tooltip.style.visibility = "hidden";
+    shown = false;
   }
 });
