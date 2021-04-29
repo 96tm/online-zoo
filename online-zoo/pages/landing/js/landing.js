0de5d8f302f0d7dbdb69096580487b3abe8b39b5
96tm <khabcity@gmail.com>
2021-04-30 00:03:26 +0300

    refactor: landing 1600px - remove grid-template-areas from pet-gallety

---
+++ online-zoo/pages/landing/js/landing.js
@@ -0,0 +1,16 @@
+const buttonLeft = document.querySelector(".pets__arrow--left");
+const buttonRight = document.querySelector(".pets__arrow--right");
+
+class Slider {
+  constructor() {
+
+  }
+}
+
+buttonLeft.addEventListener("click", event => {
+  
+});
+
+buttonRight.addEventListener("click", event => {
+
+});
