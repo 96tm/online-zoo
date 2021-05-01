c5f3722d9a52b1c0a058f3dc86361760d99ee26a
96tm <khabcity@gmail.com>
2021-05-01 18:25:28 +0300

    feat: landing 1600px - finish testimonials slider

--- online-zoo/pages/landing/js/landing.js
+++ online-zoo/pages/landing/js/landing.js
@@ -1,7 +1,10 @@
 const buttonLeft = document.querySelector(".pets__arrow--left");
 const buttonRight = document.querySelector(".pets__arrow--right");
 const sliderContainer = document.querySelector(".pets__gallery");
-const sliderList = document.querySelector(".pets__list");
+const sliderList = sliderContainer.querySelector(".pets__list");
+const testimonialsContainer = document.querySelector(".testimonials__cards-wrap");
+const testimonialsList = testimonialsContainer.querySelector(".testimonials__cards");
+const testimonialsRangeInput = document.querySelector(".testimonials__scroll-input");
 let gap = 30;
 
 class Slider {
@@ -85,3 +88,75 @@ buttonLeft.addEventListener("click", event => {
 buttonRight.addEventListener("click", event => {
   slider.scrollRight();
 });
+
+class Slider1 {
+  constructor(slider, wrap, rangeInput, gap, step, numberOfVisible) {
+    this.slider = slider;
+    this.wrap = wrap;
+    this.rangeInput = rangeInput;
+    this._gap = gap;
+    this.sliderStep = step;
+    this.numberOfVisible = numberOfVisible;
+    this.scroll = 0;
+    this.index = 0;
+    this.numberOfItems = this.slider.children.length;
+    this.slideShowHandle = null;
+    this.init();
+  }
+
+  set gap(value) {
+    this._gap = value;
+  }
+  get gap() {
+    return this._gap;
+  }
+
+  slideRight() {
+    this.index = (this.index + this.sliderStep) % (this.numberOfItems - this.numberOfVisible + 1);
+    this.scroll = (this.getCardWidth() + this.gap) * this.index;
+    this.wrap.scrollTo(this.scroll, 0);
+    this.rangeInput.value = this.index;
+  }
+  slideLeft() {
+    this.index = Math.max(0, (this.index - this.sliderStep));
+    this.scroll = (this.getCardWidth() + this.gap) * this.index;
+    this.wrap.scrollTo(this.scroll, 0);
+    this.rangeInput.value = this.index;
+
+  }
+  getCardWidth() {
+    return this.slider.firstElementChild.offsetWidth;
+  }
+  init() {
+    this.rangeInput.addEventListener("input", this.handleRangeInput.bind(this));
+    this.slideShowHandle = setInterval(this.slideRight.bind(this), 1 * 1000);
+    this.slider.addEventListener("click", event => {
+      console.log(event.currentTarget)
+      if(event.target.closest(".testimonials__list-item") && this.slideShowHandle) {
+        clearInterval(this.slideShowHandle);
+        this.slideShowHandle = null;
+        setTimeout(() => {
+          this.slideShowHandle = setInterval(this.slideRight.bind(this), 10 * 1000);
+        }, 40 * 1000);
+      }
+    });
+  }
+  handleRangeInput(event) {
+    const newValue = parseInt(event.target.value)
+    const diff = newValue - this.index;
+    console.log('change', event.target.value, 'diff', diff);
+    if (diff > 0) {
+      for (let i = 0; i < diff; i++) {
+        this.slideRight();
+      }
+    }
+    else {
+      for (let i = 0; i < -diff; i++) {
+        console.log(i, -diff)
+        this.slideLeft();
+      }
+    }
+  }
+}
+
+const s = new Slider1(testimonialsList, testimonialsContainer, testimonialsRangeInput, 30, 1, 4);
