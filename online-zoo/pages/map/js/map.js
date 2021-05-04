4bbd84cfa5231685a67bbb0ab3773bb86c5a0719
96tm <khabcity@gmail.com>
2021-05-04 23:45:04 +0300

    init: start fixing map

--- online-zoo/pages/map/js/map.js
+++ online-zoo/pages/map/js/map.js
@@ -2,11 +2,14 @@ const mapWrap = document.querySelector(".map-wrap");
 const map = mapWrap.querySelector(".map");
 const tooltip = document.querySelector(".tooltip");
 const header = document.querySelector(".header");
-const y0 = -header.getBoundingClientRect().height;
+const headerOffset = -header.getBoundingClientRect().height;
 const mapMarks = new Set(document.querySelectorAll(".map__mark"));
 const zoomButtonIn = document.querySelector(".zoom__button-in");
 const zoomButtonOut = document.querySelector(".zoom__button-out");
 
+const animalLinks = {Gorillas: 1};
+let currentTooltip = null;
+let tooltipInitialPosition = {top: 0, left: 0};
 let shown = false;
 let isDragActivated = false;
 let offset = {top: 0, left: 0};
@@ -16,40 +19,27 @@ let zoom = 1;
 
 document.addEventListener("DOMContentLoaded", event => {
   offsetY = map.getBoundingClientRect().y;
+  console.log('offsetY', offsetY)
 });
 
 map.addEventListener("mouseover", event => {
   if (event.target.classList.contains("animal-figure")
       || event.target.classList.contains("map__mark")) {
-    const animal = event.target.dataset.animal.toLowerCase();
-    const tooltip = document.querySelector(".tooltip-" + animal);
-    tooltip.style.visibility = "visible";
-    // mapMarks.forEach(v => v.classList.remove("map__mark-active"));
-    // const figureRect = event.target.getBoundingClientRect();
-    // const markId = "#map__mark-" + event.target.dataset.mapMark;
-    // const mark = document.querySelector(markId);
-    // mark.classList.add("map__mark-active");
-    // let [x, y] = [figureRect.left, y0 + event.pageY + figureRect.height];
-    // x -= tooltip.getBoundingClientRect().width / 4;
-    // console.log(tooltip.getBoundingClientRect().width / 4);
-    // const animal = mark.dataset.animal;
-    // const location = mark.dataset.location;
-    // tooltip.querySelector(".tooltip__animal").innerText = animal;
-    // tooltip.querySelector(".tooltip__location").innerText = location;
-    // tooltip.style.left = x + "px";
-    // tooltip.style.top = y + "px";
-    // tooltip.style.visibility = "visible";
-    // shown = true;
-    // console.log(x, figureRect.x, event.clientX);
+    const animal = event.target.dataset.animal;
+    const tooltip = document.querySelector(`.tooltip-${animal.toLowerCase()}`);
+    const marks = Array.from(document.querySelectorAll(`.map__mark[data-animal=${animal}]`));
+    console.log(`map__mark[data-animal=${animal}]`);
+    colorMarks(marks);
+    showTooltip(tooltip);
   }
 });
 
 map.addEventListener("mouseout", event => {
   if (event.target.classList.contains("animal-figure")
       || event.target.classList.contains("map__mark")) {
-    const animal = event.target.dataset.animal.toLowerCase();
-    const tooltip = document.querySelector(".tooltip-" + animal);
-    tooltip.style.visibility = "hidden";
+  //   const animal = event.target.dataset.animal.toLowerCase();
+  //   const tooltip = document.querySelector(".tooltip-" + animal);
+  //   tooltip.style.visibility = "hidden";
   }
 });
 
@@ -69,23 +59,80 @@ zoomButtonOut.addEventListener("mousedown", event => {
   }
 });
 
-mapWrap.addEventListener("mousedown", event => {
+mapWrap.addEventListener("pointerdown", event => {
     [startX, startY] = [event.clientX, event.clientY - offsetY];
+    console.log('down', startX, startY)
     isDragActivated = true;
 });
 
-mapWrap.addEventListener("mouseup", event => {
+mapWrap.addEventListener("pointerup", event => {
   console.log('up');
   isDragActivated  = false;
   offset.left = parseInt(map.style.left.replace("px", "")) || 0;
   offset.top = parseInt(map.style.top.replace("px", "")) || 0;
-});
-
-mapWrap.addEventListener("mousemove", event => {
-  if (isDragActivated)
-  {
-    map.style.left = offset.left + event.x - startX + "px";
-    map.style.top = offset.top + event.y - offsetY - startY + "px";
-    console.log('move', offset, event.x - startX, event.y - offsetY - startY);
+  if (currentTooltip) {
+    tooltipInitialPosition.left = currentTooltip.style.left.replace("px", "");
+    tooltipInitialPosition.top = currentTooltip.style.top.replace("px", "");
   }
 });
+
+mapWrap.addEventListener("pointermove", event => {
+  if (isDragActivated)
+  {
+    const xDistance = event.x - startX;
+    const yDistance = event.y - startY;
+    map.style.left = offset.left + xDistance + "px";
+    map.style.top = offset.top + yDistance - offsetY + "px";
+    if (currentTooltip) {
+      console.log("===moving tooltip===");
+      console.log(`map offsetY=${map.style.top}, offsetX=${map.style.left}`);
+
+      console.log(`offsetY=${offsetY}`);
+      console.log(`difference: x=${xDistance}, y=${yDistance}`);
+      console.log(`old tooltip offset: x=${currentTooltip.style.left}, y=${currentTooltip.style.top}`);
+      console.log(`old initial offset: x=${tooltipInitialPosition.left}, y=${tooltipInitialPosition.top}`);
+
+      currentTooltip.style.left = tooltipInitialPosition.left + xDistance + "px";
+      currentTooltip.style.top = tooltipInitialPosition.top + yDistance - offsetY + "px";
+      console.log(`new tooltip offset: x=${currentTooltip.style.left}, y=${currentTooltip.style.top}`);
+      console.log("======");
+    }
+  }
+});
+
+function showTooltip(tooltip) {
+  const animal = tooltip.dataset.animal;
+  const tooltips = Array.from(document.querySelectorAll(".tooltip"));
+  tooltips.forEach(v => {
+    v.classList.remove("visible");
+  });
+  tooltip.classList.add("visible");
+  const animalImage = [...map.querySelectorAll(`.animal-figure[data-animal=${animal}]`)].slice(-1)[0];
+  const imageRect = animalImage.getBoundingClientRect();
+  tooltipInitialPosition = 
+  {
+    left: imageRect.x,
+    top: imageRect.y + imageRect.height + pageYOffset + headerOffset
+  };
+  tooltip.style.left = `${tooltipInitialPosition.left}px`;
+  tooltip.style.top = `${tooltipInitialPosition.top}px`;
+  currentTooltip = tooltip;
+  console.log("===showing tooltip===");
+  console.log(`tooltip position: top=${tooltipInitialPosition.top}, left=${tooltipInitialPosition.left}`);
+  console.log(`image rect: y=${imageRect.y}, x=${imageRect.x}`);
+  console.log(`offset: top=${offset.top}, left=${offset.left}`);
+  console.log(`header offset: ${headerOffset}`);
+  console.log(`page offset: ${pageYOffset}`);
+  console.log("==========");
+
+}
+
+function colorMarks(marks) {
+  const inactiveMarks = Array.from(document.querySelectorAll(".map__mark"));
+  inactiveMarks.forEach(v => {
+    v.classList.remove("map__mark--color");
+  });
+  marks.forEach(v => {
+    v.classList.add("map__mark--color");
+  });
+}
